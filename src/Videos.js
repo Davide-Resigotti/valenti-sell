1001ad331f0033f6f3f7a80a4fa5976382375100
Davide-Resigotti <idavide.resi@gmail.com>
2025-07-11 08:56:23 +0200

    changed videos titles

--- src/Videos.js
+++ src/Videos.js
@@ -1,4 +1,3 @@
-
 import './Contents.css';
 import { Link } from 'react-router-dom';
 import { FaHome } from "react-icons/fa";
@@ -16,37 +15,37 @@ function Videos() {
       <h2 className='foto'>VIDEO</h2>
       </div>
       <div className="projectRectangles" >
-      <video alt="Prima di mettere olio nel miscelatore per fare vedere che la spia funziona, tutte le spie funzionano correttamente" controls className="rectangle" >
+      <video title="Prima di mettere olio nel miscelatore per fare vedere che la spia funziona, tutte le spie funzionano correttamente" controls className="rectangle" >
         <source src={`${process.env.PUBLIC_URL}/videos/IMG_4784.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <video alt="Video 2" controls className="rectangle" >
+      <video title="Video del motore in funzione" controls className="rectangle" >
         <source src={`${process.env.PUBLIC_URL}/videos/IMG_4785.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <video alt="Video 3" controls className="rectangle" >
+      <video title="Test funzionalità elettriche" controls className="rectangle" >
         <source src={`${process.env.PUBLIC_URL}/videos/IMG_4786.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <video alt="Video 4" controls className="rectangle" >
+      <video title="Prova di guida" controls className="rectangle" >
         <source src={`${process.env.PUBLIC_URL}/videos/IMG_4787.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <video alt="Video 5" controls className="rectangle" >
+      <video title="Test frenata" controls className="rectangle" >
         <source src={`${process.env.PUBLIC_URL}/videos/IMG_4788.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <video alt="Video 6" controls className="rectangle" >
+      <video title="Panoramica generale" controls className="rectangle" >
         <source src={`${process.env.PUBLIC_URL}/videos/IMG_4789.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
-      <video alt="Video 7" controls className="rectangle" >
+      <video title="Dettagli finali" controls className="rectangle" >
         <source src={`${process.env.PUBLIC_URL}/videos/IMG_4790.mp4`} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
